eeb8ee494e9ab815f740c969aed18a7a1dfa7532
AJ1732 <ejemeniboi@gmail.com>
2023-12-04 20:05:07 +0100

    Configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        'primary': '#215056',
+        'primary-light': '#4297AB',
+        'neutral800': '#A5ACBA',
+        'gray025': '#919BA7',
+        'gray050': '#5F6D7E',
+        'gray300': '#2E3545',
+        'gray700': '#272D37',
+        'bg-black': '#222222',
+        'text-black': '#111111',
+        'text-fade-white': '#FEFEF5',
+      },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
